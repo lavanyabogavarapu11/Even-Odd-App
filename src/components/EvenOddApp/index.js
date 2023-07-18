c6ef9b51e856a7063a4980d295ea2dfb62529130
lavanya <lavanyabogavarapu22@gmail.com>
2023-07-18 10:17:23 +0000

    Even Odd App using Conditional Rendering commit-1

---
+++ src/components/EvenOddApp/index.js
@@ -0,0 +1,44 @@
+// Write your code here
+
+import {Component} from 'react'
+
+import './index.css'
+
+class EvenOddApp extends Component {
+  state = {
+    count: 0,
+  }
+
+  getRandomNumber = () => Math.ceil(Math.random() * 100)
+
+  onClickIncrementButton = () => {
+    const randomNumber = this.getRandomNumber()
+    this.setState(prevState => ({count: prevState.count + randomNumber}))
+  }
+
+  render() {
+    const {count} = this.state
+    const displayText = count % 2 === 0 ? 'Even' : 'Odd'
+
+    return (
+      <div className="app-bg-container">
+        <div className="even-odd-container">
+          <h1 className="count-text"> Count {count} </h1>
+          <p className="msg-text"> Count is {displayText}</p>
+          <button
+            type="button"
+            className="increment-button"
+            onClick={this.onClickIncrementButton}
+          >
+            Increment
+          </button>
+          <p className="description">
+            *Increase By Random Number Between 0 to 100
+          </p>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EvenOddApp
